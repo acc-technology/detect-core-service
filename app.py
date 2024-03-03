e474b6a77dc54664d53bfcc76973b6570b557ca4
Isaray <10184500631@stu.ecnu.edu.cn>
2024-03-03 20:36:59 +0800

    feat: add db related api

--- app.py
+++ app.py
@@ -1,34 +1,108 @@
 import os
-from flask import Flask, request
+from flask import Flask, request,jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from infer_api import gen_result
+from get_gisinfo import get_location_from_image
+import time
 
 app = Flask(__name__)
-cors = CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/lcj/lab/else/detect-core-service/database.db'
 db = SQLAlchemy(app)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////" + os.path.join(
-    app.root_path, "data.db"
-)
-
-
-class Image(db.Model):
+# 创建数据库表格
+class FileInfo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    result = db.Column(db.String(100))
+    filename = db.Column(db.Text, nullable=False)
+    upload_time = db.Column(db.Integer, nullable=False)
+    longitude = db.Column(db.Float, nullable=True)
+    latitude = db.Column(db.Float, nullable=True)
+    error_types = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"<FileInfo id={self.id}, filename={self.filename}, upload_time={self.upload_time}, " \
+               f"longitude={self.longitude}, latitude={self.latitude}, error_types={self.error_types}>"
+
+# db.create_all()
+with app.app_context():
+    db.create_all()
+cors = CORS(app)
 
 
-@app.route("/upload", methods=["POST"])
-def upload():
+@app.route("/image/upload", methods=["POST"])
+def image_upload():
     image = request.files["image"]
-    image.save("../data/" + image.filename)
+    timestamp = int(time.time())
+    filename_new = f"{timestamp}_{image.filename}"
+    image.save("data/" + filename_new)
+    # print(image)
+    longitude,latitude=get_location_from_image("data/" + filename_new)
+    # exit()
+    # longitude = 0.0
+    # latitude = 0.0
+    # error_types_list=gen_result(
+    #         "./best_yolo_seg0301.pt",
+    #         image.filename,
+    #         "../data/",
+    #         "./resnet_ML_model.pth",
+    #     )
+    error_types_list=["errors1","errors2"]
+    error_types=','.join(error_types_list)
+
+    # # 将图片信息存入数据库
+    new_file_info = FileInfo(filename=filename_new, upload_time=timestamp,
+                                 longitude=longitude, latitude=latitude, error_types=error_types)
+    db.session.add(new_file_info)
+    db.session.commit()
+
     return {
         "code": 200,
-        "data": gen_result(
-            "./best_yolo_seg0301.pt",
-            image.filename,
-            "../data/",
-            "./resnet_ML_model.pth",
-        ),
+        "data":"1"
+    }
+    # return {
+    #     "code": 200,
+    #     "data": gen_result(
+    #         "./best_yolo_seg0301.pt",
+    #         image.filename,
+    #         "../data/",
+    #         "./resnet_ML_model.pth",
+    #     ),
+    # }
+@app.route("/image/list", methods=["GET"])
+def list_images():
+    images = FileInfo.query.all()
+    image_list = []
+    for image in images:
+        image_info = {
+            'id': image.id,
+            'filename': image.filename,
+            'upload_time': image.upload_time,
+            'longitude': image.longitude,
+            'latitude': image.latitude,
+            'error_types': image.error_types.split(',')  # 将错误类型分割成列表
+        }
+        image_list.append(image_info)
+    return jsonify(image_list)
+
+@app.route("/image/get", methods=["GET"])
+def get_image():
+    image_id = request.args.get('id')
+    image = FileInfo.query.filter_by(id=image_id).first()
+    if image:
+        image_info = {
+            'id': image.id,
+            'filename': image.filename,
+            'upload_time': image.upload_time,
+            'longitude': image.longitude,
+            'latitude': image.latitude,
+            'error_types': image.error_types.split(',')  # 将错误类型分割成列表
+        }
+        return jsonify(image_info)
+    else:
+        return jsonify({'error': 'Image not found'}), 404
+    
+@app.route("/test", methods=["POST"])
+def test():
+    return {
+        "code": 200,
+        "data": "1",
     }
---
+++ get_gisinfo.py
@@ -0,0 +1,50 @@
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+def get_geotagging(image):
+    exif_data = image._getexif()
+    if exif_data is not None:
+        for tag, value in exif_data.items():
+            tag_name = TAGS.get(tag, tag)
+            if tag_name == 'GPSInfo':
+                return value
+    return None
+
+def get_decimal_from_dms(dms):
+    degrees = dms[0]
+    minutes = dms[1]
+    seconds = dms[2]
+    return degrees + (minutes / 60.0) + (seconds / 3600.0)
+# def dms_to_decimal(degrees, minutes, seconds):
+#     decimal = degrees + (minutes / 60.0) + (seconds / 3600.0)
+#     return decimal
+
+def get_lat_lon(geotags):
+    if geotags is None:
+        return None, None
+    lat_dms = geotags.get(2)
+    lon_dms = geotags.get(4)
+    if lat_dms is None or lon_dms is None:
+        return None, None
+    lat = get_decimal_from_dms(lat_dms)
+    lon = get_decimal_from_dms(lon_dms)
+    return lat, lon
+
+def get_location_from_image(image_path):
+    try:
+        image = Image.open(image_path)
+        # print(1)
+        geotags = get_geotagging(image)
+        lat, lon = get_lat_lon(geotags)
+        return float(lat), float(lon)
+    except Exception as e:
+        print(f"Error: {e}")
+        return 0.0, 0.0
+
+# 示例用法
+# image_path = "/home/lcj/lab/else/sam_model/blind_data_split2/train/images/0000503.jpg"
+# latitude, longitude = get_location_from_image(image_path)
+# if latitude is not None and longitude is not None:
+#     print(f"经度: {longitude}, 纬度: {latitude}")
+# else:
+#     print("未找到位置信息。")
